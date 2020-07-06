05b1d7226eb3620131b8b325b90443dd77a368ce
empireV <vovaempire@gmail.com>
2020-07-06 21:39:15 +0300

    added models

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -8,7 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class CommentsComponent implements OnInit {
 
   @Input()
-  comment: any;
+  comment: Comment;
 
   constructor() { }
 
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {User} from '../../models/User';
 
 @Component({
   selector: 'app-user',
@@ -8,7 +9,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class UserComponent implements OnInit {
 
   @Input()
-  user: any;
+  user: User;
 
   constructor() {
   }
